9fc9d25d550bdd2e6310d5299290460b5d716fc0
Shubham <shubhamgore112@gmail.com>
2022-06-13 17:53:25 +0530

    Added several functions

--- script.js
+++ script.js
@@ -1,14 +1,86 @@
 const display = document.getElementById("display");
 const buttonsPanel = document.getElementById("buttons-panel");
-const buttonsAll = document.querySelectorAll(".buttons");
+const buttonsOperator = document.querySelectorAll(".operator");
+const buttonsOperand = document.querySelectorAll(".operand");
+const clrDel = document.querySelectorAll(".delete-clear-button");
+// console.log(buttonsOperand);
 
-// creating buttons-panel childs
-// for(let i=0;i<16;i++){
-//     const button = document.createElement("div");
-//     button.classList.add("buttons");
-//     // button.innerHTML(i);
-//     buttonsPanel.append(button);
-// }
+// initialization
+let firstNo = "";
+let secondNo = "";
+
+// buttonsOperator.forEach((button)=>{
+//     changeDisplay(button);
+// });
+// buttonsOperand.forEach((button) => {
+//     button.addEventListener('click', () => operandFunc(button.value));}
+// );
+
+for(let i=0; i<buttonsOperand.length; i++){
+    // console.log(buttonsOperand[i]);
+    changeDisplay(buttonsOperand[i]);
+}
+
+for(let i=0; i<buttonsOperator.length; i++){
+    operatorFunc(buttonsOperator[i]);
+}
+
+for(let i=0; i<clrDel.length; i++){
+    if(clrDel[i]=="clr"){
+        clrScreen(clrDel[i]);
+    }
+    else{
+        backspace(clrDel[i]);
+    }
+}
+
+// clear screen
+function clrScreen(button){
+    button.addEventListener("click",function(){
+        firstNo = "";
+        secondNo = "";
+        op="";
+        display.innerHTML="";
+        console.log("clr executed");
+    })
+}
+function backspace(button){
+    console.log("del executed");
+}
+
+// adding digits to display
+function changeDisplay(button){
+    // console.log(button);
+    button.addEventListener("click",function(){
+    let buttonVal = button.getAttribute("value");
+    displayNumber(buttonVal);
+    });
+}
+
+function operatorFunc(button){
+
+    button.addEventListener("click",function(){
+        if(button=="="){
+            console.log("= execited");
+            secondNo = display.innerHTML;
+            let answer = operator(op,firstNo,secondNo);
+            console.log(answer);
+            display.innerHTML=answer;
+        }else{
+            console.log("else executed");
+        op = button;
+        firstNo = display.innerHTML;
+        display.innerHTML="";
+        }
+    });   
+}
+
+
+function displayNumber(buttonVal){
+    display.innerHTML+=buttonVal;
+}
+
+
 
 // basic operation functions
 function add(x,y){
@@ -45,8 +117,20 @@ function operator(op,x,y){
 }
 
 // function to change display value
-function populate_display(val){
-    display.textContent=val;
-}
+// function populate_display(val){
+//     display.textContent=val;
+// }
 
-populate_display(10);
+
+// else if(buttonVal=="="){
+//     let currNumber = display.innerHTML;
+//     display.innerHTML="";
+//     operator(op,currNumber,prevNumber);
+
+// }
+// else{
+// let prevNumber = display.innerHTML;
+// display.innerHTML="";
+// let op = buttonVal;
+
+// }
